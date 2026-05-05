e10f5b41061cfb5cd75b304ab8b1b4a2b16da331
leonardo <leonardomalevo@gmail.com>
2026-05-04 22:18:01 -0300

    revirtiendo merge

--- Database/migrations/001_crear_clientes.sql
+++
@@ -1,9 +0,0 @@
--- Migración 001: Crear tabla clientes
-CREATE TABLE clientes (
-    cliente_id SERIAL PRIMARY KEY,
-    nombre VARCHAR(200) NOT NULL,
-    email VARCHAR(200) UNIQUE NOT NULL,
-    telefono VARCHAR(50),
-    direccion TEXT,
-    creado_en TIMESTAMP WITH TIME ZONE DEFAULT NOW()
-);
--- Program.cs
+++ Program.cs
@@ -1,7 +1,2 @@
-﻿using System;
-
-var connectionString = Environment.GetEnvironmentVariable("BOXSERVICE_CONNECTION_STRING")
-    ?? "Host=localhost;Port=5432;Username=postgres;Password=admin;Database=boxservice";
-
-var server = new Server(connectionString);
-await server.StartAsync();
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
